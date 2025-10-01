7b4c8455e01dbbc22af967d9b8361d0ac9a90863
Flash-Night <138298913+Flash-Night@users.noreply.github.com>
2025-10-01 22:20:39 +0800

    test#switch scenes

--- main.gd
+++ main.gd
@@ -4,7 +4,8 @@ extends Control
 
 func _ready():
 	startbutton.text = "Click me"
-	startbutton.pressed.connect(_button_pressed)
+	startbutton.pressed.connect(_start)
 
-func _button_pressed():
-	print("Hello world!")
+func _start():
+	print("helloworld")
+	get_tree().change_scene_to_file("res://game.tscn")
