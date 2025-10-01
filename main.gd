2b1e75d0eef6573a46d99f757705c7734c748597
Flash-Night <138298913+Flash-Night@users.noreply.github.com>
2025-10-01 23:33:23 +0800

    test#instantiate, json

--- main.gd
+++ main.gd
@@ -1,11 +1,25 @@
 extends Control
 
 @export var startbutton:Button
+@export var endbutton:Button
+
+var mybutton:PackedScene
+var count:int = 0
 
 func _ready():
-	startbutton.text = "Click me"
+	#startbutton.text = "Click me"
 	startbutton.pressed.connect(_start)
+	endbutton.pressed.connect(_createMyButton)
+	mybutton = load("res://mybutton.tscn")
 
 func _start():
 	print("helloworld")
 	get_tree().change_scene_to_file("res://game.tscn")
+
+func _createMyButton():
+	if count < 10:
+		var mbinst = mybutton.instantiate()
+		add_child(mbinst)
+		mbinst.position.x = 50
+		mbinst.position.y = 70 * count
+		count += 1
