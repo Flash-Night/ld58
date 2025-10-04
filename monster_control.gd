d30580807b37e99e4c42eb2b19eb0de9f93a0eb0
TsuyuriKuraudo <hyt0_1_1_2_3_5_8@yahoo.com>
2025-10-04 18:27:06 +0800

    generate tile butttons

---
+++ monster_control.gd
@@ -0,0 +1,25 @@
+extends Control
+@onready var player=$"../Player"
+@onready var camera=$"../Camera2D"
+var buttons:Array[Button]
+var scene=preload("res://tile_button.tscn")
+func _ready() -> void:
+	var control=camera.get_node("Control")
+	control.game_control=self
+	control.bconnect()
+func place_tile_buttons()->void:
+	remove_tile_buttons()
+	var x = player.position.x
+	var y = player.position.y
+	for i in range(7):
+		for j in range(7):
+			buttons.append(scene.instantiate())
+			var k=i*7+j
+			add_child(buttons[k])
+			buttons[k].position.x=x-7*32+i*64
+			buttons[k].position.y=y-7*32+j*64
+			
+func remove_tile_buttons()->void:
+	for b in buttons:
+		b.queue_free()
+	buttons.resize(0)
