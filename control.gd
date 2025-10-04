ea41a9bfed83d62e26a6e7e701223d7e6a259d36
TsuyuriKuraudo <hyt0_1_1_2_3_5_8@yahoo.com>
2025-10-04 17:13:47 +0800

    generate ui butttons

---
+++ control.gd
@@ -0,0 +1,20 @@
+extends Control
+
+var selecting:int
+var buttons:Array[Button]
+var max_using = 8
+func _ready() -> void:
+	var scene=preload("res://monster_using.tscn")
+	buttons.resize(max_using)
+	for i in range(max_using):
+		buttons[i] = scene.instantiate()
+		add_child(buttons[i])
+		buttons[i].position.x=-600+i*100
+		buttons[i].position.y=250
+
+
+func _process(delta: float) -> void:
+	selecting=0
+	for i in range(max_using):
+		if buttons[i].button_pressed:
+			selecting=i
